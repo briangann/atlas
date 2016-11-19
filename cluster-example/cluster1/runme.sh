3e357128c3b963cf3e11c5762478fa3f2a0717ab
Brian Gann <brian.gann@ge.com>
2016-11-18 20:02:40 -0600

    add use of shared journal

--- cluster-example/cluster1/runme.sh
+++ cluster-example/cluster1/runme.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -jar ../../target/standalone.jar ./cluster1.conf
+java -jar ../../target/standalone.jar ./cluster1-shared-journal.conf
+#java -jar ../../target/standalone.jar ./cluster1.conf
